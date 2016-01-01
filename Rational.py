aef37d0db9d84b3026d02cf27fda2c43d7046bc0
JasonGUTU <115010148@link.cuhk.edu.cn>
2016-01-01 22:54:00 +0800

    2016/1/1

--- Rational.py
+++ Rational.py
@@ -7,19 +7,6 @@
 #********************************************************
 # A class for Rational Numbers
 class Rational:
-	def gcd(n,d):
-		n1 = abs(n)
-		n2 = abs(d)
-		gcd = 1
-
-		k = 1
-		while k <= n1 and k <= n2:
-			if n1 % k == 0 and n2 % k == 0:
-				gcd = k
-			k += 1
-		
-		return gcd
-
 	def __init__(self,numerator = 0,denominator = 1):
 		divisor = gcda(numerator,denominator)
 		self.__numerator = (1 if denominator > 0 else -1) * int(numerator / divisor)
