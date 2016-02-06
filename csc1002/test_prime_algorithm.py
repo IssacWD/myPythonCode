569e547b929207979f31b79f7b5b134d984264c1
JasonGUTU <115010148@link.cuhk.edu.cn>
2016-02-06 23:15:16 +0800

    2016/2/5

--- csc1002/test_prime_algorithm.py
+++ csc1002/test_prime_algorithm.py
@@ -6,23 +6,59 @@
 #	> Time   : 2016/2/5
 #********************************************************
 
+
 def deco_time(func):
     def in_deco_time(n):
         import time
         start = time.clock()
-        func(n)
+        number_of_prime = func(n)
         end = time.clock()
         cost_time = (end - start) * 1000
-        return cost_time
+        return cost_time, number_of_prime
     return in_deco_time
 
-@deco_time
-def algorithm_Eratosthenes(number):
+
+def algorithm_Eratosthenes(max_number):
+    if max_number < 2:
+        return [], 0
+    list_of_number = [i for i in range(3, max_number, 2)]
+    for number in list_of_number:
+        for i in range(2, max_number // number):
+            try:
+                list_of_number.remove(number * i)
+            except ValueError:
+                continue
+    return list_of_number, len(list_of_number)
 
 
-@deco_time
-def algorithm_Judge(number):
+
+def Judge(number):
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+    for i in range(3, int(number ** 0.5) + 1, 1):
+        if number % i == 0:
+            return False
+    return True
 
 
-@deco_time
-def algorithm_fusion_judge_erato(number):
+# def algorithm_fusion_judge_erato(number):
+
+
+# @deco_time
+def algorithm_Judge(max_number):
+    if max_number < 2:
+        return [], 0
+    if max_number == 2:
+        return [2], 1
+    list_of_prime = [2]
+    for i in range(3, max_number, 2):
+        if Judge(i):
+            list_of_prime.append(i)
+    return list_of_prime, len(list_of_prime)
+
+# def main():
+for i in algorithm_Eratosthenes(10000)[0]:
+    if i not in algorithm_Judge(10000)[0]:
+        print(i)
