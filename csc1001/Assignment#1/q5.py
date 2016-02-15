419d4c168230f5bf35dd060a6c5f4382e0dc35e3
JasonGUTU <115010148@link.cuhk.edu.cn>
2016-02-15 16:58:31 +0800

    2016/2/15

--- csc1001/Assignment#1/q5.py
+++ csc1001/Assignment#1/q5.py
@@ -6,14 +6,44 @@
 #	> Time   : 2016/2/5
 #********************************************************
 # The Question Five
+
+
 def get_number_and_judge():
     number = input("Enter an integer:")
     if not number.isdigit():
         print("Wrong input")
         number = get_number_and_judge()
-    if eval(number) < 2:
+    if eval(number) <= 2:
         print("Don't have any number.")
         number = get_number_and_judge()
     return int(number)
 
 
+def Judge(number):
+    for i in range(3, int(number ** 0.5) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+
+def prime_list(Number):
+    prime_list_ = [2]
+    for i in range(3, Number, 2):
+        if Judge(i):
+            prime_list_.append(i)
+    return prime_list_
+
+
+def display(list):
+    print(len(list))
+    for i in range(len(list)):
+        if i % 7 == 0:
+            print(format(list[i], '<8d'), end='\n')
+        else:
+            print(format(list[i], '<8d'), end='')
+
+
+
+N = get_number_and_judge()
+print('The prime numbers smaller than %s include:' % N)
+display(prime_list(N))
---
+++ csc1001/Assignment#1/q6.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+#********************************************************
+#   > OS     : Ubuntu 14.04
+#	> Author : JasonGUTU
+#	> Mail   : intergujinjin@foxmail.com
+#	> Time   : 2016/2/15
+#********************************************************
+# The Question Six
+
+
+import math
+
+
+def get_func():
+    func = input("Input the function:")
+    if func == 'sin':
+        return math.sin
+    elif func == 'cos':
+        return math.cos
+    elif func == 'tan':
+        return math.tan
+    else:
+        print("Does not support this function!\nPlease input one of 'sin' 'cos' 'tan'")
+        return get_func()
+
+
+def get_a_b_n():
+    a = eval(input("Input a:"))
+    b = eval(input("Input b:"))
+    if b <= a:
+        a = eval(input("a should be smaller than b\nPlease input a again:"))
+    n = int(input("Input the number of sub-intervals:"))
+    middleware = (b - a) / n
+    return middleware, n, a
+
+
+def calculate(func, tuple):
+    midware = tuple[0]
+    n = tuple[1]
+    a = tuple[2]
+    _sum = 0
+    for i in range(n):
+        _sum += func(a + midware * (i - 0.5))
+    return midware * _sum
+
+
+if __name__ == '__main__':
+    print("The result is %s" % calculate(get_func(), get_a_b_n()))
