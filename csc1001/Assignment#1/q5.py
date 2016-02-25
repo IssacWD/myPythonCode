49d54a4f1559b71f09f0c20d91fbeff0bf952f9c
JasonGUTU <115010148@link.cuhk.edu.cn>
2016-02-26 02:08:34 +0800

    2016/2/26

--- csc1001/Assignment#1/q5.py
+++ csc1001/Assignment#1/q5.py
@@ -7,28 +7,31 @@
 #********************************************************
 # The Question Five
 from AssTools.getInput import get_number
-
-def Judge(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-    return True
+import time
 
 
-def prime_list(number):
-    prime_list_ = [2]
-    for i in range(3, number):
-        if Judge(i):
-            prime_list_.append(i)
-    return prime_list_
+def algorithm_Eratosthenes(max_number):
+    import math
+    list_of_bool = [True] * (max_number + 1)
+    list_of_bool[0] = list_of_bool[1] = False
+    for i in range(2, math.ceil(math.sqrt(max_number))):
+        if not list_of_bool[i]:
+            continue
+        for j in range(i * i, max_number + 1, i):
+            list_of_bool[j] = False
+    return list_of_bool
 
 
-def display(list):
-    for i in range(len(list)):
+def display(list_of_bool):
+    prime_number_list = list()
+    for i in range(len(list_of_bool)):
+        if list_of_bool[i]:
+            prime_number_list.append(i)
+    for i in range(len(prime_number_list)):
         if (i + 1) % 8 == 0:
-            print(format(list[i], '<8d'), end='\n')
+            print(format(prime_number_list[i], '<9d'), end='\n')
         else:
-            print(format(list[i], '<8d'), end='')
+            print(format(prime_number_list[i], '<9d'), end='')
 
 
 if __name__ == '__main__':
@@ -37,4 +40,12 @@ if __name__ == '__main__':
         print('Do not have any number that meet the requirements!')
     else:
         print('The prime numbers smaller than %s include:' % number)
-        display(prime_list(number))
+        a = time.clock()
+        _list = algorithm_Eratosthenes(number)
+        b = time.clock()
+        display(_list)
+        c = time.clock()
+        print('\n %s \n %s \n %s' % (b - a, c - b, c - a))
+
+
+
